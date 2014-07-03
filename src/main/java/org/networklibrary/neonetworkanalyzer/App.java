94d3dbd3440091d2b5e0d71d5f6d302937e84b22
Georg Summer <georg.summer@gmail.com>
2014-07-03 21:08:18 +0200

    updated to match linux

--- src/main/java/org/networklibrary/neonetworkanalyzer/App.java
+++ src/main/java/org/networklibrary/neonetworkanalyzer/App.java
@@ -12,45 +12,11 @@ import org.networklibrary.neonetworkanalyzer.neo4jalgos.NeoAnalyzerImpl;
  */
 public class App 
 {
-<<<<<<< HEAD
-    public static void main( String[] args )
-    {
-    	// mac os x
-//    	String testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.1.1/data/graph.db";
-//        testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.0.3_testing/data/graph.db";
-
-        
-        // proper OS
-        
-        final String testing = "/home/gsu/random/neoanalyzer/testing/data/graph.db";
-//        final String full = "/home/gsu/random/neoanalyzer/full/data/graph.db";
-        
-        GraphDatabaseService g = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(
-                testing )
-                .setConfig( GraphDatabaseSettings.read_only, "true" )
-                .newGraphDatabase();
-//        GraphDatabaseService g = new GraphDatabaseFactory().newEmbeddedDatabase(dbloc);
-//        GraphDatabaseService g = new GraphDatabaseFactory().newEmbeddedDatabase(full);
-        
-        System.out.println("starting to analyze:");
-        NeoAnalyzerImpl analyzer = new NeoAnalyzerImpl();
-        
-        long start = System.currentTimeMillis();
-        List<String> res = analyzer.analyze(g);
-        long end = System.currentTimeMillis();
-        
-        System.out.println("duration: " + (end - start));
-        
-        for(String nodeProps : res){
-        	System.out.println(nodeProps);
-        }
-    }
-=======
 	public static void main( String[] args )
 	{
 		// mac os x
 		//    	String testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.1.1/data/graph.db";
-		String testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.1.1-2/data/graph.db";
+//		String testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.1.1-2/data/graph.db";
 		//        testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.0.3_testing/data/graph.db";
 
 
@@ -58,6 +24,8 @@ public class App
 
 		//        final String testing = "/home/gsu/random/neoanalyzer/testing/data/graph.db";
 		//        final String full = "/home/gsu/random/neoanalyzer/full/data/graph.db";
+		
+		String testing = "/home/gsu/random/benchmarking/neo4j-community-2.1.2/data/graph.db";
 
 		GraphDatabaseService g = new GraphDatabaseFactory().newEmbeddedDatabase(testing);
 
@@ -74,5 +42,4 @@ public class App
 			System.out.println(nodeProps);
 		}
 	}
->>>>>>> cd77b1e9ed48c8e0af94cacd98bf578b639a9bb7
 }
