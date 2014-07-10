7b6817b447bd107d8d1739e3a0bdb1be8c377d55
Georg Summer <georg.summer@gmail.com>
2014-07-10 20:15:56 +0200

    added a main class for command like operations
    benchmark functionality in the main class
    
    playing around with ideas for optimization and general info output for benchmark checking

--- src/main/java/org/networklibrary/neonetworkanalyzer/App.java
+++ src/main/java/org/networklibrary/neonetworkanalyzer/App.java
@@ -1,9 +1,11 @@
 package org.networklibrary.neonetworkanalyzer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.networklibrary.neonetworkanalyzer.neo4jalgos.NeoAnalyzerImpl;
 
 /**
@@ -14,36 +16,47 @@ public class App
 {
 	public static void main( String[] args )
 	{
-		// mac os x
-		//    	String testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.1.1/data/graph.db";
-//		String testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.1.1-2/data/graph.db";
-		//        testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.0.3_testing/data/graph.db";
-		String testing = "/Users/gsu/random/neonetworkanalyzer/neo4j-community-2.1.2/data/graph.db";
-
-//		        testing = "/Users/gsu/random/cynetlibsync/neo4j-community-2.0.3_testing/data/graph.db";
-
-
-		// proper OS
-
-//		        final String testing = "/home/gsu/random/neoanalyzer/testing/data/graph.db";
-		//        final String full = "/home/gsu/random/neoanalyzer/full/data/graph.db";
-		
-//		String testing = "/home/gsu/random/benchmarking/neo4j-community-2.1.2/data/graph.db";
-//		String testing = "/home/gsu/random/benchmarking/bigger/data/graph.db";
-
-		GraphDatabaseService g = new GraphDatabaseFactory().newEmbeddedDatabase(testing);
-
-		System.out.println("starting to analyze:");
-		NeoAnalyzerImpl analyzer = new NeoAnalyzerImpl();
-
-		long start = System.currentTimeMillis();
-		List<String> res = analyzer.analyze(g,true);
-		long end = System.currentTimeMillis();
-
-		for(String nodeProps : res){
-			System.out.println(nodeProps);
+		if(args.length != 2){
+			System.out.println("not enough arguments; need graph and numRuns");
+			return;
 		}
+
+		String graphloc = args[0];
+		int numRuns = Integer.parseInt(args[1]);
 		
-		System.out.println("duration: " + (end - start));
+		List<Long> durations = new ArrayList<Long>();
+		
+		GraphDatabaseService g = new GraphDatabaseFactory().newEmbeddedDatabase(graphloc);
+		
+//		GraphDatabaseService g = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(graphloc)
+//				.setConfig(GraphDatabaseSettings.nodestore_mapped_memory_size,"400M")
+//				.setConfig(GraphDatabaseSettings.relationshipstore_mapped_memory_size,"400M")
+//				.newGraphDatabase();
+		
+		for(int i = 0; i < numRuns; ++i){
+
+			System.out.println("starting to analyze:");
+			NeoAnalyzerImpl analyzer = new NeoAnalyzerImpl();
+
+			long start = System.currentTimeMillis();
+			List<String> res = analyzer.analyze(g,true);
+			long end = System.currentTimeMillis();
+
+			//		for(String nodeProps : res){
+			//			System.out.println(nodeProps);
+			//		}
+
+			System.out.println("duration\t" + (end - start));
+			durations.add((end-start));
+			
+		}
+		g.shutdown();
+		
+		long sum = 0;
+		for(Long duration : durations){
+			sum += duration;
+			System.out.println("duration\t"+duration);
+		}
+		System.out.println("average duration\t"+sum/(double)durations.size());
 	}
 }
--- src/main/java/org/networklibrary/neonetworkanalyzer/neo4jalgos/NeoAnalyzerImpl.java
+++ src/main/java/org/networklibrary/neonetworkanalyzer/neo4jalgos/NeoAnalyzerImpl.java
@@ -56,7 +56,7 @@ public class NeoAnalyzerImpl implements NeoAnalyzer {
 				types = Iterables.toArray(RelationshipType.class,GlobalGraphOperations.at(graph).getAllRelationshipTypes());
 				tx.success();
 			}
-			
+
 			SingleSourceShortestPath<Integer> sssPath = new SingleSourceShortestPathDijkstra<Integer>(0, null, new CostEvaluator<Integer>(){
 				@Override
 				public Integer getCost(Relationship relationship, Direction direction) {
@@ -74,7 +74,7 @@ public class NeoAnalyzerImpl implements NeoAnalyzer {
 
 			try (Transaction tx = graph.beginTx()){
 				ParallellCentralityCalculation<Integer> ppc = new ParallellCentralityCalculation<Integer>(sssPath, component);
-				
+
 				ppc.addCalculation(eccentricity);
 				ppc.addCalculation(betweennessCentrality);
 				ppc.addCalculation(stressCentrality);
@@ -91,34 +91,34 @@ public class NeoAnalyzerImpl implements NeoAnalyzer {
 			Radiality<Integer> radiality = new Radiality<>(maxEccentricity, avgSP);
 
 			int currNodeI = 0;
-			
+
 			for(Node node : component){
-//				System.out.println("starting on node finishing up: " + currNodeI + " " + node);
+				//				System.out.println("starting on node finishing up: " + currNodeI + " " + node);
 				try (Transaction tx = graph.beginTx()){
 					Map<String, Object> stats = new HashMap<String,Object>();
 
 					Iterable<Relationship> rels  = node.getRelationships();
 					long edgecount = Iterables.count(rels);
 
-//					stats.put("neo_edgecount", edgecount);
-//					stats.put("neo_indegree", Iterables.count(node.getRelationships(Direction.INCOMING)));
-//					stats.put("neo_outdegree", Iterables.count(node.getRelationships(Direction.OUTGOING)));
-//					stats.put("neo_issinglenode", (edgecount==0) ? true : false);
+					//					stats.put("neo_edgecount", edgecount);
+					//					stats.put("neo_indegree", Iterables.count(node.getRelationships(Direction.INCOMING)));
+					//					stats.put("neo_outdegree", Iterables.count(node.getRelationships(Direction.OUTGOING)));
+					//					stats.put("neo_issinglenode", (edgecount==0) ? true : false);
 
 					if(edgecount == 0){
 
 						//						stats.put("neo_name", node.getProperty("name","unknown"));
-//						stats.put("neo_betweenness", 0.0);
-//						stats.put("neo_name", node.getProperty("name","unknown"));
-//						stats.put("neo_betweenness", 0.0);
+						//						stats.put("neo_betweenness", 0.0);
+						//						stats.put("neo_name", node.getProperty("name","unknown"));
+						//						stats.put("neo_betweenness", 0.0);
 						stats.put("neo_stresscentrality", 0.0);
-//						stats.put("neo_closenesscentrality", 0.0);
-//						stats.put("neo_eccentriticy", 0);
-////						stats.put("neo_avgSP", 0.0);
-//						stats.put("neo_clustcoeff", 0.0);
-//						stats.put("neo_neighbourhoodconnectivity",0.0);
-//						stats.put("neo_multiedgepairs",0L);
-//						stats.put("neo_topologicalcoeff", 0.0);
+						//						stats.put("neo_closenesscentrality", 0.0);
+						//						stats.put("neo_eccentriticy", 0);
+						////						stats.put("neo_avgSP", 0.0);
+						//						stats.put("neo_clustcoeff", 0.0);
+						//						stats.put("neo_neighbourhoodconnectivity",0.0);
+						//						stats.put("neo_multiedgepairs",0L);
+						//						stats.put("neo_topologicalcoeff", 0.0);
 
 					} else {
 						Set<Node> thisNode = new HashSet<Node>();
@@ -129,19 +129,19 @@ public class NeoAnalyzerImpl implements NeoAnalyzer {
 						double closeness = (avgsp > 0) ? (1/avgsp) : 0.0;
 
 
-//						stats.put("neo_indegree", Iterables.count(node.getRelationships(Direction.INCOMING)));
-//						stats.put("neo_outdegree", Iterables.count(node.getRelationships(Direction.OUTGOING)));
-//						//						stats.put("neo_name", node.getProperty("name","unknown"));
-//						stats.put("neo_betweenness", betweenness);
+						//						stats.put("neo_indegree", Iterables.count(node.getRelationships(Direction.INCOMING)));
+						//						stats.put("neo_outdegree", Iterables.count(node.getRelationships(Direction.OUTGOING)));
+						//						//						stats.put("neo_name", node.getProperty("name","unknown"));
+						//						stats.put("neo_betweenness", betweenness);
 						stats.put("neo_stresscentrality", stressCentrality.getCentrality(node));
-////						stats.put("neo_closenesscentrality", closeness);
-//						stats.put("neo_eccentriticy", eccentricity.getCentrality(node));
-////						stats.put("neo_avgSP", avgsp);
-//						stats.put("neo_clustcoeff", clustCoeff.calcClusteringCoeff(node));
-//						stats.put("neo_neighbourhoodconnectivity",neighbourhoodConn.calcNeighbourhoodConnectivity(node));
-//						stats.put("neo_multiedgepairs",multiEdgePairs.calcMultipleEdgePairs(node));
-//						stats.put("neo_topologicalcoeff", topoCoeff.calcTopologicalCoeff(node));
-////						stats.put("neo_radiality", radiality.calcRadiality(node));
+						////						stats.put("neo_closenesscentrality", closeness);
+						//						stats.put("neo_eccentriticy", eccentricity.getCentrality(node));
+						////						stats.put("neo_avgSP", avgsp);
+						//						stats.put("neo_clustcoeff", clustCoeff.calcClusteringCoeff(node));
+						//						stats.put("neo_neighbourhoodconnectivity",neighbourhoodConn.calcNeighbourhoodConnectivity(node));
+						//						stats.put("neo_multiedgepairs",multiEdgePairs.calcMultipleEdgePairs(node));
+						//						stats.put("neo_topologicalcoeff", topoCoeff.calcTopologicalCoeff(node));
+						////						stats.put("neo_radiality", radiality.calcRadiality(node));
 
 					}
 
@@ -162,8 +162,8 @@ public class NeoAnalyzerImpl implements NeoAnalyzer {
 					tx.success();
 				}
 
-//				System.out.println("finished on node finishing up: " + currNodeI + " " + node);
-		        ++currNodeI;
+				//				System.out.println("finished on node finishing up: " + currNodeI + " " + node);
+				++currNodeI;
 			}
 			System.out.println("finished with component " + currComp);
 			++currComp;
@@ -192,6 +192,8 @@ public class NeoAnalyzerImpl implements NeoAnalyzer {
 		components = new ArrayList<Set<Node>>();
 		Set<Node> visited = new HashSet<Node>();
 
+		int currStep = 0;
+		
 		for(Node n : GlobalGraphOperations.at(graph).getAllNodes()){
 			if(visited.contains(n))
 				continue;
@@ -212,7 +214,14 @@ public class NeoAnalyzerImpl implements NeoAnalyzer {
 
 				visited.add(curr);
 				currComponent.add(curr);
+
 				q.addAll(NetworkUtils.getUniqueNeighbours(curr));
+				++currStep;
+
+				if(currStep % 1000 == 0){
+					System.out.println("current step = "+ currStep);
+				}
+				
 			}
 
 			components.add(currComponent);
